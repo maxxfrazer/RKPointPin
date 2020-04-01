ed3ae0e1bd11e235c3457c7933349053cfdf2fb6
Max Cobb <5754073+maxxfrazer@users.noreply.github.com>
2020-04-01 21:22:05 +0100

    package tidy up

--- Package.swift
+++ Package.swift
@@ -4,26 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "RKPointPin",
-    platforms: [.iOS(.v13)],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "RKPointPin",
-            targets: ["RKPointPin"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "RKPointPin",
-            dependencies: []),
-        .testTarget(
-            name: "RKPointPinTests",
-            dependencies: ["RKPointPin"]),
-    ]
+  name: "RKPointPin",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(name: "RKPointPin", targets: ["RKPointPin"])
+  ],
+  targets: [
+    .target(name: "RKPointPin", dependencies: []),
+    .testTarget(name: "RKPointPinTests", dependencies: ["RKPointPin"])
+  ]
 )
